4740e66b8f8053d082ef855b717688067d7515bd
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2023-11-27 17:11:09 +0700

    Update package.json and yarn.lock

--- client/src/pages/customer-registrations-page.tsx
+++ client/src/pages/customer-registrations-page.tsx
@@ -1,18 +1,28 @@
 import { FadeIn } from "@/components/common/framer-motion";
 import { Icons } from "@/components/common/icons";
 import { Button } from "@/components/ui/button";
-// import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import {
   Table,
-  //   TableCaption,
   TableBody as TBody,
   TableCell as Td,
   TableHead as Th,
   TableHeader as THead,
   TableRow as Tr,
 } from "@/components/ui/table";
+import { Textarea } from "@/components/ui/textarea";
 import registrationsMock from "@/mock/registrations.mock.json";
 import { RegistrationInterface } from "@/models";
+import {
+  FontBoldIcon,
+  FontItalicIcon,
+  StrikethroughIcon,
+  TextAlignCenterIcon,
+  TextAlignLeftIcon,
+  TextAlignRightIcon,
+} from "@radix-ui/react-icons";
+import * as Toolbar from "@radix-ui/react-toolbar";
 import { FC } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -34,17 +44,132 @@ const CustomerRegistrations: FC = () => {
             <span>ลงทะเบียนลูกค้าใหม่</span>
             <Icons.plus className="h-8 w-8 font-bold" />
           </Button>
-          <Button className="flex items-center justify-between px-[1rem] py-[2rem] sm:w-[12rem] lg:w-[20rem]">
-            <span>ลงทะเบียนลูกค้าใหม่ (ส่งเมล)</span>
-            <Icons.plus className="h-8 w-8 font-bold" />
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="flex items-center justify-between px-[1rem] py-[2rem] sm:w-[12rem] lg:w-[20rem]">
+                <span>ลงทะเบียนลูกค้าใหม่ (ส่งเมล)</span>
+                <Icons.plus className="h-8 w-8 font-bold" />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="flex w-[70vw] max-w-[70vw] flex-col gap-y-2">
+              <section className="flex flex-col gap-y-1">
+                <article className="flex items-center gap-x-1">
+                  <h2 className="text-md font-semibold">
+                    ลงทะเบียนลูกค้าใหม่ (ส่งเมล)
+                  </h2>
+                </article>
+                <article className="flex flex-col gap-2 rounded-md">
+                  <section className="rounded bg-secondary p-2">
+                    <p className="text-sm">
+                      กรุณากรอกข้อมูลลงทะเบียนลูกค้าใหม่ให้ครบถ้วน
+                    </p>
+                  </section>
+                  <section className="flex flex-col gap-2">
+                    <div className="grid grid-cols-10 gap-x-2">
+                      {/* <div className="col-span-1 flex items-center justify-end">
+                        <h3 className="text-sm font-semibold">ผู้ส่ง</h3>
+                      </div> */}
+                      <Input
+                        className="col-span-10 w-full px-4"
+                        type="email"
+                        variant="flushed"
+                        placeholder="ผู้ส่ง"
+                        defaultValue="Anuwat_Thisuka@gmail.com"
+                      />
+                    </div>
+                    <div className="grid grid-cols-10 gap-x-2">
+                      {/* <div className="col-span-1 flex items-center justify-end">
+                        <h3 className="text-sm font-semibold">เรื่อง</h3>
+                      </div> */}
+                      <Input
+                        className="col-span-10 w-full px-4"
+                        type="email"
+                        variant="flushed"
+                        placeholder="เรื่อง"
+                        defaultValue="ขอข้อมูลการลงทะเบียน"
+                      />
+                    </div>
+                  </section>
+                  <section>
+                    <Toolbar.Root
+                      className="flex w-full min-w-max rounded-none border-b bg-white p-[10px]"
+                      aria-label="Formatting options"
+                    >
+                      <Toolbar.ToggleGroup
+                        type="multiple"
+                        aria-label="Text formatting"
+                      >
+                        <Toolbar.ToggleItem
+                          className="text-mauve11 hover:bg-violet3 hover:text-violet11 focus:shadow-violet7 data-[state=on]:bg-violet5 data-[state=on]:text-violet11 ml-0.5 inline-flex h-[25px] flex-shrink-0 flex-grow-0 basis-auto items-center justify-center rounded bg-white px-[5px] text-[13px] leading-none outline-none first:ml-0 focus:relative focus:shadow-[0_0_0_2px]"
+                          value="bold"
+                          aria-label="Bold"
+                        >
+                          <FontBoldIcon />
+                        </Toolbar.ToggleItem>
+                        <Toolbar.ToggleItem
+                          className="text-mauve11 hover:bg-violet3 hover:text-violet11 focus:shadow-violet7 data-[state=on]:bg-violet5 data-[state=on]:text-violet11 ml-0.5 inline-flex h-[25px] flex-shrink-0 flex-grow-0 basis-auto items-center justify-center rounded bg-white px-[5px] text-[13px] leading-none outline-none first:ml-0 focus:relative focus:shadow-[0_0_0_2px]"
+                          value="italic"
+                          aria-label="Italic"
+                        >
+                          <FontItalicIcon />
+                        </Toolbar.ToggleItem>
+                        <Toolbar.ToggleItem
+                          className="text-mauve11 hover:bg-violet3 hover:text-violet11 focus:shadow-violet7 data-[state=on]:bg-violet5 data-[state=on]:text-violet11 ml-0.5 inline-flex h-[25px] flex-shrink-0 flex-grow-0 basis-auto items-center justify-center rounded bg-white px-[5px] text-[13px] leading-none outline-none first:ml-0 focus:relative focus:shadow-[0_0_0_2px]"
+                          value="strikethrough"
+                          aria-label="Strike through"
+                        >
+                          <StrikethroughIcon />
+                        </Toolbar.ToggleItem>
+                      </Toolbar.ToggleGroup>
+                      <Toolbar.Separator className="bg-mauve6 mx-[10px] w-[1px]" />
+                      <Toolbar.ToggleGroup
+                        type="single"
+                        defaultValue="center"
+                        aria-label="Text alignment"
+                      >
+                        <Toolbar.ToggleItem
+                          className="text-mauve11 hover:bg-violet3 hover:text-violet11 focus:shadow-violet7 data-[state=on]:bg-violet5 data-[state=on]:text-violet11 ml-0.5 inline-flex h-[25px] flex-shrink-0 flex-grow-0 basis-auto items-center justify-center rounded bg-white px-[5px] text-[13px] leading-none outline-none first:ml-0 focus:relative focus:shadow-[0_0_0_2px]"
+                          value="left"
+                          aria-label="Left aligned"
+                        >
+                          <TextAlignLeftIcon />
+                        </Toolbar.ToggleItem>
+                        <Toolbar.ToggleItem
+                          className="text-mauve11 hover:bg-violet3 hover:text-violet11 focus:shadow-violet7 data-[state=on]:bg-violet5 data-[state=on]:text-violet11 ml-0.5 inline-flex h-[25px] flex-shrink-0 flex-grow-0 basis-auto items-center justify-center rounded bg-white px-[5px] text-[13px] leading-none outline-none first:ml-0 focus:relative focus:shadow-[0_0_0_2px]"
+                          value="center"
+                          aria-label="Center aligned"
+                        >
+                          <TextAlignCenterIcon />
+                        </Toolbar.ToggleItem>
+                        <Toolbar.ToggleItem
+                          className="text-mauve11 hover:bg-violet3 hover:text-violet11 focus:shadow-violet7 data-[state=on]:bg-violet5 data-[state=on]:text-violet11 ml-0.5 inline-flex h-[25px] flex-shrink-0 flex-grow-0 basis-auto items-center justify-center rounded bg-white px-[5px] text-[13px] leading-none outline-none first:ml-0 focus:relative focus:shadow-[0_0_0_2px]"
+                          value="right"
+                          aria-label="Right aligned"
+                        >
+                          <TextAlignRightIcon />
+                        </Toolbar.ToggleItem>
+                      </Toolbar.ToggleGroup>
+                      <Toolbar.Separator className="bg-mauve6 mx-[10px] w-[1px]" />
+                    </Toolbar.Root>
+                  </section>
+                  <section>
+                    <Textarea
+                      className="h-[30rem] max-h-[30rem] w-full border-none shadow-none focus-visible:ring-0"
+                      placeholder="เนื้อหา"
+                      defaultValue={`เรียน คุณ สมชาย ใจดี\nผู้จัดการบริษัท จูลิสติกส์ จำกัด \nที่อยู่ 123 ถนน สุขุมวิท แขวง คลองเตย เขต คลองเตย กรุงเทพมหานคร 10110\nโทรศัพท์ 02-123-4567\nโทรสาร 02-123-4567\nมือถือ 081-123-4567 อีเมล์ `}
+                    />
+                  </section>
+                  <section className="flex items-center justify-end gap-x-2">
+                    <Button className="w-1/12">ส่งเมล</Button>
+                  </section>
+                </article>
+              </section>
+            </DialogContent>
+          </Dialog>
         </div>
 
         <div className="mt-5 w-full flex-grow overflow-x-auto overflow-y-auto rounded border">
           <Table className="relative w-full">
-            {/* <Table className="relative w-full border-separate"> */}
-            {/* <table className="relative w-full border-separate"> */}
-            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
             <THead className="sticky top-0 z-10 whitespace-nowrap bg-primary-foreground text-sm">
               <Tr>
                 <Th className="w-[100px]">ลำดับ</Th>
