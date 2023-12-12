3174bed187db7451b09cd187c4ddea689b45a1c5
Suttawee Chatpattanasiri <98304076+net253@users.noreply.github.com>
2023-12-12 11:17:57 +0700

    Update CompanyController.php

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -16,6 +16,20 @@ class CompanyController extends Controller
         $this->jwtUtils = new JWTUtils();
     }
 
+
+    //!random Name
+    private function randomName(int $length = 10)
+    {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_';
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = \strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < $length; $i++) {
+            $n = \rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return \implode($pass); //turn the array into a string
+    }
+
     //!Add Company
     function addCompany(Request $request)
     {
@@ -31,6 +45,8 @@ class CompanyController extends Controller
 
             \date_default_timezone_set('Asia/Bangkok');
             $now = new \DateTime();
+            $datetime = date('Y-m-d H:i:s');
+            $path = \getcwd() . "\\..\\..\\docs\\pdf\\";
 
 
             $rules = [
@@ -52,48 +68,91 @@ class CompanyController extends Controller
                 'share_holder.thai_nationalities' => 'required|integer|min:0|max:100',
                 'share_holder.other_nationalities' => 'required|integer|min:0|max:100',
 
-                'contact_person.*.position' => 'required|string',
+                'contact_person.*.position_th' => 'required|string',
+                'contact_person.*.position_en' => 'required|string',
                 'contact_person.*.name' => 'required|string',
-                'contact_person.*.phone_number' => 'required|string',
+                'contact_person.*.tel' => 'required|string',
                 'contact_person.*.email' => 'required|email',
 
                 'relationship.is_relationship' => 'required|boolean',
                 'relationship.relationship_name' => 'required_if:relationship.is_relationship,true|string',
 
-                'standard.certificate.*.name' => 'required|string',
-                'standard.certificate.*.label' => 'required|string',
-                'standard.certificate.*.labelEN' => 'required|string',
-                'standard.certificate.*.isChecked' => 'required|boolean',
+                'standard.certificate.*.label_th' => 'required|string',
+                'standard.certificate.*.label_en' => 'required|string',
+                'standard.certificate.*.is_checked' => 'required|boolean',
                 'standard.certificate.*.value' => 'required|string',
                 'standard.certificate.*.exp' => 'required|string',
-                'standard.benefit.*.name' => 'required|string',
-                'standard.benefit.*.label' => 'required|string',
-                'standard.benefit.*.labelEN' => 'required|string',
-                'standard.benefit.*.isChecked' => 'required|boolean',
+                'standard.benefit.*.label_th' => 'required|string',
+                'standard.benefit.*.label_en' => 'required|string',
+                'standard.benefit.*.is_checked' => 'required|boolean',
                 'standard.benefit.*.value' => 'required|string',
                 'standard.benefit.*.exp' => 'required|string',
                 'payment_term.credit_term.name' => 'required|string',
-                'payment_term.credit_term.value' => 'required|string',
+                'payment_term.credit_term.value' => 'required|integer',
                 'payment_term.billing_term.name' => 'required|string',
                 'payment_term.billing_term.value' => 'required|string',
                 'payment_term.currency' => 'required|string',
                 'payment_term.incoterm' => 'required|string',
                 'payment_term.L/C_term.is_L/C' => 'required|boolean',
                 'payment_term.L/C_term.L/C_type' => 'required|string',
-                'payment_term.delivery_term.*.label_th' => 'required|string',
-                'payment_term.delivery_term.*.label_en' => 'required|string',
-                'payment_term.delivery_term.*.is_checked' => 'required|boolean',
+                'payment_term.*.delivery_term.*.label_th' => 'required|string',
+                'payment_term.*.delivery_term.*.label_en' => 'required|string',
+                'payment_term.*.delivery_term.*.is_checked' => 'required|boolean',
+                'payment_term.deposit_term.is_deposit' => 'required|boolean',
+                'payment_term.deposit_term.deposit_type' => 'required|string',
+                'payment_term.product_warranty.is_warranty' => 'required|boolean',
+                'payment_term.product_warranty.value' => 'required|string',
+                'payment_term.company_policy.*.label_th' => 'required|string',
+                'payment_term.company_policy.*.label_en' => 'required|string',
+                'payment_term.company_policy.*.is_checked' => 'required|boolean',
+                'payment_term.objective_purchasing.name' => 'required|string',
+                'payment_term.objective_purchasing.value' => 'required|string',
+                'payment_term.main_customer.name' => 'required|string',
+                'payment_term.main_customer.value' => 'required|string',
             ];
 
 
             $validator = Validator::make($request->all(), $rules);
             if ($validator->passes()) {
-                $company_information = $request->company_information;
-                $share_holder = $request->share_holder;
-                $contact_person = $request->contact_person;
-                $standard = $request->standard;
-                $relationship = $request->relationship;
-                $payment_term = $request->payment_term;
+                $company_information = json_encode($request->company_information);
+                $share_holder = json_encode($request->share_holder);
+                $contact_person = json_encode($request->contact_person);
+                $standard = json_encode($request->standard);
+                $relationship = json_encode($request->relationship);
+                $payment_term = json_encode($request->payment_term);
+                $documents = json_encode($request->documents);
+                $document_id = $request-> document_id;
+                $anticorruptionpolicy = $request-> anticorruptionpolicy;
+
+            //     $chkData = DB::table('dev_company_informations')
+            //     ->where('company_id', $company_id)
+            //     ->take(1)
+            //     ->get();
+
+            //     $validator2 = count($chkData) == 0;
+            // if ($validator2) return $this->response->setStatusCode(400)->setJSON(["state" => false, "msg" => "เงื่อนไขการร้องขอผิดพลาด", "msgEN" => "Request condition error"]);
+            $timestamp = $now->getTimestamp();
+            $folder_name = $this->randomName(10) . (string)$timestamp;
+
+            $token_file = $this->randomName(100);
+
+            $gen_dir = $path . $folder_name;
+
+            if (!is_dir($gen_dir)) \mkdir($gen_dir);
+
+                $filesData = array(
+                    "document_id" => $document_id,
+                    "folder_name" => $folder_name,
+                );
+
+                if (strlen($anticorruptionpolicy) > 200) {
+                    $fileName = $token_file . "-anticorruption-policy.pdf";
+                    $filesData["STDPackingFileName"] = $fileName;
+                    $genPath = $path . $folder_name . "\\" . $fileName;
+                    $base64 = \trim($anticorruptionpolicy, "data:application/pdf;base64,");
+                    file_put_contents($genPath, \base64_decode($base64));
+                }
+
 
                 $result = Company::insert([
                     "company_information" => $company_information,
@@ -102,6 +161,8 @@ class CompanyController extends Controller
                     "standard" => $standard,
                     "relationship" => $relationship,
                     "payment_term" => $payment_term,
+                    "create_at" => $now,
+                    "update_at" => $now
                 ]);
 
                 return response()->json([
@@ -134,8 +195,8 @@ class CompanyController extends Controller
     }
 
 
-    //Edit Device
-    function editDevice(Request $request)
+    //!company_list
+    function companyList(Request $request)
     {
         try {
             $header = $request->header('Authorization');
@@ -145,124 +206,49 @@ class CompanyController extends Controller
                 "message" => "Unauthorized",
                 "data" => [],
             ], 401);
-            // $decoded = $jwt->decoded;
+            // $validators = Validator::make(
+            $request->all();
+            //     [
+            //         'username' => 'required|string',
+            //         'password' => 'required|string|min:1|max:255',
+            //     ]
+            // );
 
-            \date_default_timezone_set('Asia/Bangkok');
-            $update_datetime = new \DateTime();
+            // if ($validators->fails()) {
+            //     return response()->json([
+            //         "status" => "error",
+            //         "message" => "Bad request",
+            //         "data" => [
+            //             [
+            //                 "validator" => $validators->errors()
+            //             ]
+            //         ]
+            //     ], 400);
+            // }
 
-            $validator = Validator::make(
-                $request->all(),
-                [
-                    'serial_no'     => 'required|string|min:1|max:255',
-                    'device_name'   => 'required|string|min:1|max:255',
-                    // 'ip_address'    => 'required|string|min:7|max:15',
-                    'status'        => 'required|string|min:1|max:15',
-                    // 'remarks'       => 'required|string|min:0|max:255',
-                    // 'is_active'     => 'required|boolean',
-                ]
-            );
-            if ($validator->fails()) {
-                return response()->json([
-                    "status" => "error",
-                    "message" => "Bad request",
-                    "data" => [
-                        [
-                            "validator" => $validator->errors()
-                        ]
-                    ]
-                ], 400);
-            }
-
-            $serial_no = $request->serial_no;
-            $device_name = $request->device_name;
-            $ip_address = $request->ip_address;
-            $status = $request->status;
-            $remarks = $request->remarks;
-            $is_active = $request->is_active;
-
-            if ($status === "broken") {
-                $new_name = $device_name . '' . now()->format('Y-m-d h:i:sa');
-
-                $result = Company::where('serial_no', $serial_no)->update([
-                    'serial_no'     => $serial_no,
-                    'device_name'   => $new_name,
-                    'status'        => $status,
-                    'remarks'       => $remarks,
-                    'is_active' => $is_active
-                ]);
-            } else {
-                $result = Company::where('serial_no', $serial_no)->update([
-                    'serial_no'     => $serial_no,
-                    'device_name'   => $device_name,
-                    'status'        => $status,
-                    'remarks'       => $remarks,
-                    'is_active' => $is_active
-                ]);
-            }
-
-
-
-
-
-
-
-
-
-
+            $data = DB::table("dev_company_list")->take(1)->get();
+            if (\count($data) == 0) return response()->json([
+                "status" => "error",
+                "message" => "There is no data company in the iCRS system",
+                "data" => []
+            ]);
 
             return response()->json([
-                "status" => 'success',
-                "message" => "Edited device successfully",
-                "data" => [
-                    [
-                        "result" => $update_datetime
-                    ]
-                ],
+                "status" => "success",
+                "message" => "data output success",
+                "data" => $data
             ]);
         } catch (\Exception $e) {
             return response()->json([
                 "status" => "error",
                 "message" => $e->getMessage(),
-                "data" => [],
+                "data" => []
             ], 500);
         }
     }
 
-    // Delete Decvice
-    // function deleteDevice($serial_no, Request $request)
-    // {
-    //     try {
-    //         $header = $request->header('Authorization');
-    //         $jwt = $this->jwtUtils->verifyToken($header);
-    //         if (!$jwt->state) return response()->json([
-    //             "status" => "error",
-    //             "message" => "Unauthorized",
-    //             "data" => [],
-    //         ], 401);
-    //         // $decoded = $jwt->decoded;
-
-    //         $result = Device::where('serial_no', $serial_no)->delete();
-
-    //         return response()->json([
-    //             "status" => 'success',
-    //             "message" => "Deleted device successfully",
-    //             "data" => [
-    //                 [
-    //                     "result" => $result
-    //                 ]
-    //             ],
-    //         ]);
-    //     } catch (\Exception $e) {
-    //         return response()->json([
-    //             "status" => "error",
-    //             "message" => $e->getMessage(),
-    //             "data" => [],
-    //         ], 500);
-    //     }
-    // }
-
-    //* [GET] /devices/get-all-devices
-    function getAllDevices(Request $request)
+    //!business_type
+    function businessType(Request $request)
     {
         try {
             $header = $request->header('Authorization');
@@ -272,47 +258,64 @@ class CompanyController extends Controller
                 "message" => "Unauthorized",
                 "data" => [],
             ], 401);
-            // $decoded = $jwt->decoded;
 
-            // $result = Devices::orderBy("serial_no")->orderByDesc("import_at")->get();
-            // $result = Devices::selectRaw(
-            //     "serial_no
-            //     ,device_name
-            //     ,status
-            //     ,remarks
-            //     ,battery
-            //     ,ip_address
-            //     ,last_actived_at::varchar(19) as last_actived_at
-            //     ,last_actived_at::timestamp::varchar(19) as last_actived_at
-            //     ,created_at::varchar(19) as _created_at
-            //     ,updated_at::varchar(19) as _updated_at
-            //     "
-            // )->orderBy("created_at")->get();
+            $request->all();
 
-            $result = DB::table('devices')->selectRaw(
-                "serial_no
-                ,device_name
-                ,status
-                ,remarks
-                ,battery
-                ,ip_address
-                ,last_actived_at::varchar(19) as last_actived_at
-                ,last_actived_at::timestamp::varchar(19) as last_actived_at
-                ,created_at::varchar(19) as created_at
-                ,updated_at::varchar(19) as updated_at
-                "
-            )->orderBy('created_at')->get();
+            $data = DB::table("dev_business_type")->get();
+            if (\count($data) == 0) return response()->json([
+                "status" => "error",
+                "message" => "There is no data business type in the iCRS system",
+                "data" => []
+            ]);
 
             return response()->json([
-                "status" => 'success',
-                "message" => "OK",
-                "data" => $result,
+                "status" => "success",
+                "message" => "data output success",
+                "data" => $data
             ]);
         } catch (\Exception $e) {
             return response()->json([
                 "status" => "error",
                 "message" => $e->getMessage(),
+                "data" => []
+            ], 500);
+        }
+    }
+
+    //!business_type
+    function countryAmount(Request $request)
+    {
+        try {
+
+            $header = $request->header('Authorization');
+            $jwt = $this->jwtUtils->verifyToken($header);
+            if (!$jwt->state) return response()->json([
+                "status" => "error",
+                "message" => "Unauthorized",
                 "data" => [],
+            ], 401);
+
+            $request->all();
+
+            $data = DB::table("dev_country")->get();
+            if (\count($data) == 0) return response()->json([
+                "status" => "error",
+                "message" => "There is no data country in the iCRS system",
+                "data" => []
+            ]);
+
+            return response()->json([
+                "status" => "success",
+                "message" => "data output success",
+                "data" => [
+                    "data" => $data
+                ]
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => "error",
+                "message" => $e->getMessage(),
+                "data" => []
             ], 500);
         }
     }
