97d8eab36f22621fb69f370892ffdb786a908436
Suttawee Chatpattanasiri <98304076+net253@users.noreply.github.com>
2023-12-12 15:59:34 +0700

    Add files via upload

---
+++ app/Models/Documents.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Documents extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'dev_documents';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The data type of the primary key.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'document_id';
+    protected $fillable = [
+        "folder_name",
+        "documents",
+    ];
+}
