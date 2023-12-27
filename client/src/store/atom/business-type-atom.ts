572a08d3f6fec5e6599d9baaefb9f83a355f4111
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2023-12-27 21:16:21 +0700

    Remove mock data from businessTypeAtom

--- client/src/store/atom/business-type-atom.ts
+++ client/src/store/atom/business-type-atom.ts
@@ -1,8 +1,8 @@
-import MockBusinessTypeList from "@/mocks/business-type.json";
+// import MockBusinessTypeList from "@/mocks/business-type.json";
 import { TBusinessTypeList } from "@/types";
 import { atom } from "jotai";
 
-export const businessTypeAtom = atom<TBusinessTypeList[]>(MockBusinessTypeList);
+export const businessTypeAtom = atom<TBusinessTypeList[]>([]);
 
 if (process.env.NODE_ENV !== "production") {
   businessTypeAtom.debugLabel = "businessTypeAtom";
