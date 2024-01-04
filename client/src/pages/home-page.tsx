a0963b04e1ba6ccf819ff44636f46f1f7d935ad6
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2024-01-04 11:22:29 +0700

    Update user authentication logic

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -40,17 +40,14 @@ const HomePage: FC = () => {
       name: "ซื้อมาขายไป",
       value: 0,
       ซื้อมาขายไป:
-        dataObjectivePurchasingRatio?.filter(
-          ({ objective_purchasing }) => objective_purchasing === "trade",
-        )?.[0]?.amount ?? 10,
+        dataObjectivePurchasingRatio?.filter(({ objective_purchasing }) => objective_purchasing === "trade")?.[0]
+          ?.amount ?? 10,
       ผลิตงาน:
-        dataObjectivePurchasingRatio?.filter(
-          ({ objective_purchasing }) => objective_purchasing === "produce",
-        )?.[0]?.amount ?? 10,
+        dataObjectivePurchasingRatio?.filter(({ objective_purchasing }) => objective_purchasing === "produce")?.[0]
+          ?.amount ?? 10,
       อื่นๆ:
-        dataObjectivePurchasingRatio?.filter(
-          ({ objective_purchasing }) => objective_purchasing === "other",
-        )?.[0]?.amount ?? 10,
+        dataObjectivePurchasingRatio?.filter(({ objective_purchasing }) => objective_purchasing === "other")?.[0]
+          ?.amount ?? 10,
       datetime: "วัตถุประสงค์การซื้อสินค้า",
     },
   ];
@@ -59,19 +56,13 @@ const HomePage: FC = () => {
     {
       id: "ไทย",
       label: "ไทย",
-      value:
-        dataMainCustomerRatio?.filter(
-          ({ main_customer }) => main_customer === "internal",
-        )?.[0]?.amount ?? 0,
+      value: dataMainCustomerRatio?.filter(({ main_customer }) => main_customer === "internal")?.[0]?.amount ?? 0,
       color: "#1F2C57",
     },
     {
       id: "ต่างประเทศ",
       label: "ต่างประเทศ",
-      value:
-        dataMainCustomerRatio?.filter(
-          ({ main_customer }) => main_customer === "foreign",
-        )?.[0]?.amount ?? 0,
+      value: dataMainCustomerRatio?.filter(({ main_customer }) => main_customer === "foreign")?.[0]?.amount ?? 0,
       color: "#F2C94C",
     },
   ];
@@ -92,33 +83,24 @@ const HomePage: FC = () => {
     }, 0);
 
   const TotolRegisWest = dataRegisCount
-    ?.map(({ regis_count, province }) =>
-      province !== "RAYONG" ? 0 : regis_count ?? 0,
-    )
+    ?.map(({ regis_count, province }) => (province !== "RAYONG" ? 0 : regis_count ?? 0))
     ?.reduce((accumulator, currentValue) => {
       return accumulator + currentValue;
     }, 0);
   const TotolRegisEast = dataRegisCount
-    ?.map(({ regis_count, province }) =>
-      province !== "SAMUTPAKAN" ? 0 : regis_count ?? 0,
-    )
+    ?.map(({ regis_count, province }) => (province !== "SAMUTPAKAN" ? 0 : regis_count ?? 0))
     ?.reduce((accumulator, currentValue) => {
       return accumulator + currentValue;
     }, 0);
 
   const dataRegisWest = dataRegisCount
-    ?.filter(
-      ({ province, regis_count }) => province !== "RAYONG" && regis_count > 0,
-    )
+    ?.filter(({ province, regis_count }) => province !== "RAYONG" && regis_count > 0)
     ?.map(({ company, regis_count }) => ({
       name: company,
       children: [{ name: company, size: regis_count }],
     }));
   const dataRegisEast = dataRegisCount
-    ?.filter(
-      ({ province, regis_count }) =>
-        province !== "SAMUTPAKAN" && regis_count > 0,
-    )
+    ?.filter(({ province, regis_count }) => province !== "SAMUTPAKAN" && regis_count > 0)
     ?.map(({ company, regis_count }) => ({
       name: company,
       children: [{ name: company, size: regis_count }],
@@ -132,16 +114,12 @@ const HomePage: FC = () => {
       <section className="flex items-center justify-between gap-2">
         <h1 className="text-xl font-semibold">
           ภาพรวมของการขึ้นทะเบียนผู้ซื้อ{" "}
-          <span className="text-sm text-primary">
-            (อัพเดทล่าสุดเมื่อวันที่:{" "}
-            {
-              getDateThai(
-                dataCountRegisList.length > 0 &&
-                  dataCountRegisList[0]?.created_at,
-              ).dateTime
-            }
-            )
-          </span>
+          {dataCountRegisList.length > 0 && (
+            <span className="text-sm text-primary">
+              (อัพเดทล่าสุดเมื่อวันที่:{" "}
+              {getDateThai(dataCountRegisList.length > 0 && dataCountRegisList[0]?.created_at).dateTime})
+            </span>
+          )}
         </h1>
         <div className="w-44">
           <Select className="text-xs shadow-none" defaultValue="2024">
@@ -152,19 +130,15 @@ const HomePage: FC = () => {
       <section className="grid h-full grid-cols-8 grid-rows-2 gap-2">
         <article className="col-span-6 row-span-1 flex h-full flex-col">
           {dataRegisStat?.length === 0 ? (
-            <article className="flex h-full w-full flex-col items-center justify-center">
+            <article className="flex h-full w-full flex-col items-center justify-center rounded-md border">
               <text className="text-sm">
-                {isFetchingRegisCount
-                  ? "กำลังโหลดข้อมูล..."
-                  : "ยังไม่มีข้อมูลการลงทะเบียน"}
+                {isFetchingRegisCount ? "กำลังโหลดข้อมูล..." : "ยังไม่มีข้อมูลการลงทะเบียน"}
               </text>
             </article>
           ) : (
             <section className="grid h-full w-full grid-cols-8 grid-rows-2 gap-2 ">
               <article className="col-span-2 row-span-2 flex h-full flex-col items-center justify-center rounded-md border p-2">
-                <h3 className="text-md font-bold">
-                  จำนวนลูกค้า SNC รวมทั้งเครือ
-                </h3>
+                <h3 className="text-md font-bold">จำนวนลูกค้า SNC รวมทั้งเครือ</h3>
                 <h1
                   className={`${
                     TotolRegis < 1000
@@ -180,9 +154,7 @@ const HomePage: FC = () => {
                 </h1>
               </article>
               <article className="relative z-10 col-span-2 flex h-full flex-col items-center  justify-center rounded-md border p-2">
-                <h3 className="text-md absolute left-0 top-[-10px] bg-white px-2 font-bold">
-                  East :
-                </h3>
+                <h3 className="text-md absolute left-0 top-[-10px] bg-white px-2 font-bold">East :</h3>
                 <h1
                   // className={`${
                   //   TotolRegis < 1000
@@ -200,7 +172,7 @@ const HomePage: FC = () => {
                       ? "text-[4rem]"
                       : TotolRegis < 100000
                       ? "text-[3rem]"
-                      : "text-[2rem]",
+                      : "text-[2rem]"
                   )}
                 >
                   {TotolRegisEast?.toLocaleString("en")}
@@ -210,9 +182,7 @@ const HomePage: FC = () => {
                 <TreeMap data={dataRegisEast} />
               </article>
               <article className="relative col-span-2 flex h-full flex-col items-center justify-center rounded-md border p-2">
-                <h3 className="text-md absolute left-0 top-[-10px] bg-white px-2 font-bold">
-                  West :
-                </h3>
+                <h3 className="text-md absolute left-0 top-[-10px] bg-white px-2 font-bold">West :</h3>
                 <h1
                   // className={`${
                   //   TotolRegis < 1000
@@ -230,7 +200,7 @@ const HomePage: FC = () => {
                       ? "text-[4rem]"
                       : TotolRegis < 100000
                       ? "text-[3rem]"
-                      : "text-[2rem]",
+                      : "text-[2rem]"
                   )}
                 >
                   {TotolRegisWest?.toLocaleString("en")}
@@ -250,9 +220,7 @@ const HomePage: FC = () => {
             {dataMainCustomerRatio?.length === 0 ? (
               <article className="flex h-full w-full flex-col items-center justify-center">
                 <text className="text-sm">
-                  {isFetchingShareHolderRatio
-                    ? "กำลังโหลดข้อมูล..."
-                    : "ยังไม่มีข้อมูลการลงทะเบียน"}
+                  {isFetchingShareHolderRatio ? "กำลังโหลดข้อมูล..." : "ยังไม่มีข้อมูลการลงทะเบียน"}
                 </text>
               </article>
             ) : (
@@ -260,9 +228,7 @@ const HomePage: FC = () => {
                 <article className="flex h-[99%] w-full flex-col  items-center justify-center">
                   <PieChartComponents
                     data={Datanationality?.reduce((acc: any, cur) => {
-                      const index = acc.findIndex(
-                        (item: { id: string }) => item.id === cur.id,
-                      );
+                      const index = acc.findIndex((item: { id: string }) => item.id === cur.id);
                       if (index === -1) {
                         acc.push({
                           id: cur.id || "",
@@ -286,9 +252,7 @@ const HomePage: FC = () => {
                 </article>
                 <article className="absolute bottom-0 right-0">
                   {Datanationality?.reduce((acc: any, cur) => {
-                    const index = acc.findIndex(
-                      (item: { id: string }) => item.id === cur.id,
-                    );
+                    const index = acc.findIndex((item: { id: string }) => item.id === cur.id);
                     if (index === -1) {
                       acc.push({
                         id: cur.id || "",
@@ -302,29 +266,19 @@ const HomePage: FC = () => {
                     return acc;
                   }, [])
                     ?.slice(0, 5)
-                    ?.map(
-                      (
-                        { id, color }: { id: string; color: string },
-                        i: number,
-                      ) => (
-                        <article
-                          key={i}
-                          className="flex flex-row items-center justify-start gap-2"
-                        >
-                          <div
-                            className={"h-3 w-3 rounded-full"}
-                            style={{
-                              backgroundColor: color,
-                            }}
-                          />
-                          <h6 className="text-xs font-bold">{id}</h6>
-                        </article>
-                      ),
-                    )}
+                    ?.map(({ id, color }: { id: string; color: string }, i: number) => (
+                      <article key={i} className="flex flex-row items-center justify-start gap-2">
+                        <div
+                          className={"h-3 w-3 rounded-full"}
+                          style={{
+                            backgroundColor: color,
+                          }}
+                        />
+                        <h6 className="text-xs font-bold">{id}</h6>
+                      </article>
+                    ))}
                   {Datanationality?.length > 5 && (
-                    <h6 className="mt-[-6px] w-full text-center text-xs font-bold">
-                      ...
-                    </h6>
+                    <h6 className="mt-[-6px] w-full text-center text-xs font-bold">...</h6>
                   )}
                 </article>
               </>
@@ -332,15 +286,11 @@ const HomePage: FC = () => {
           </section>
         </article>
         <article className="col-span-2 row-span-1 flex flex-col gap-4 rounded-md border p-2">
-          <h3 className="text-md font-bold">
-            ประเภทลูกค้าแบ่งตามวัตถุประสงค์การซื้อสินค้า
-          </h3>
+          <h3 className="text-md font-bold">ประเภทลูกค้าแบ่งตามวัตถุประสงค์การซื้อสินค้า</h3>
           {dataObjectivePurchasingRatio?.length === 0 ? (
             <article className="flex h-[92%] w-full flex-col items-center justify-center">
               <text className="text-sm">
-                {isFetchingObjectivePurchasingRatio
-                  ? "กำลังโหลดข้อมูล..."
-                  : "ยังไม่มีข้อมูลการลงทะเบียน"}
+                {isFetchingObjectivePurchasingRatio ? "กำลังโหลดข้อมูล..." : "ยังไม่มีข้อมูลการลงทะเบียน"}
               </text>
             </article>
           ) : (
@@ -360,16 +310,12 @@ const HomePage: FC = () => {
         <article className="relative col-span-4 row-span-1 rounded-md border">
           <section className="flex h-full w-full flex-col gap-2 p-2">
             <article>
-              <h3 className="text-md font-bold">
-                ประวัติขึ้นทะเบียนผู้ซื้อแบ่งตามบริษัท{" "}
-              </h3>
+              <h3 className="text-md font-bold">ประวัติขึ้นทะเบียนผู้ซื้อแบ่งตามบริษัท </h3>
             </article>
             {data?.length === 0 ? (
               <article className="flex h-full w-full flex-col items-center justify-center">
                 <text className="text-sm">
-                  {isFetchingRegisCount
-                    ? "กำลังโหลดข้อมูล..."
-                    : "ยังไม่มีข้อมูลการลงทะเบียน"}
+                  {isFetchingRegisCount ? "กำลังโหลดข้อมูล..." : "ยังไม่มีข้อมูลการลงทะเบียน"}
                 </text>
               </article>
             ) : (
@@ -387,15 +333,11 @@ const HomePage: FC = () => {
           </section>
         </article>
         <article className="col-span-2 row-span-1 flex h-full flex-col rounded-md border p-2">
-          <h3 className="text-md font-bold">
-            ลูกค้าหลักของบริษัทที่ขึ้นทะเบียน
-          </h3>
+          <h3 className="text-md font-bold">ลูกค้าหลักของบริษัทที่ขึ้นทะเบียน</h3>
           {dataMainCustomerRatio?.length === 0 ? (
             <article className="flex h-full w-full flex-col items-center justify-center">
               <text className="text-sm">
-                {isFetchingMainCustomerRatio
-                  ? "กำลังโหลดข้อมูล..."
-                  : "ยังไม่มีข้อมูลการลงทะเบียน"}
+                {isFetchingMainCustomerRatio ? "กำลังโหลดข้อมูล..." : "ยังไม่มีข้อมูลการลงทะเบียน"}
               </text>
             </article>
           ) : (
@@ -413,10 +355,7 @@ const HomePage: FC = () => {
               </article>
               <article className="absolute bottom-6 right-0">
                 {DataMainCustomers?.map(({ id, color }, i) => (
-                  <article
-                    key={i}
-                    className="flex flex-row items-center justify-start gap-2"
-                  >
+                  <article key={i} className="flex flex-row items-center justify-start gap-2">
                     <div
                       className={"h-3 w-3 rounded-full"}
                       style={{
