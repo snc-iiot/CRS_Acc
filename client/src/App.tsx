7448bf2cefcd1955fd5453942fb568a418bc90b1
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2024-01-04 09:30:34 +0700

    Update routing and API base URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,6 +16,8 @@ import {
 function App() {
   return (
     <>
+      {/* <BrowserRouter basename={"/icrs/admin/dev"}> */}
+      {/* <BrowserRouter basename={"/icrs/admin/uat"}> */}
       <BrowserRouter basename={"/icrs/admin"}>
         {/* <BrowserRouter> */}
         <Routes>
@@ -23,21 +25,12 @@ function App() {
           <Route path="/" element={<RootLayout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/test-page" element={<TestPage />} />
-            <Route
-              path="/registrations/customer/register"
-              element={<Registration />}
-            />
+            <Route path="/registrations/customer/register" element={<Registration />} />
             <Route path="/registrations" element={<CustomerRegistrations />} />
             <Route path="/settings" element={<SettingPage />} />
-            <Route
-              path="/settings/evaluation-config"
-              element={<EvaluationConfigPage />}
-            />
+            <Route path="/settings/evaluation-config" element={<EvaluationConfigPage />} />
           </Route>
-          <Route
-            path="/registrations/customer/info"
-            element={<RegistrationInfo />}
-          />
+          <Route path="/registrations/customer/info" element={<RegistrationInfo />} />
           <Route path="/server-error" element={<ServerErrorPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
