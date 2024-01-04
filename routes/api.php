f43d595a5b19b7493e122feb906971507751ce6e
sataporn1995 <satapornchai2538@gmail.com>
2024-01-04 14:47:21 +0700

    Update clean code

--- routes/api.php
+++ routes/api.php
@@ -112,14 +112,7 @@ Route::prefix('dashboard')->controller(DashboardController::class)->group(functi
     Route::get('/objective-purchasing-ratio', 'objectivePurchasingRatio');
     Route::get('/regis-stat', 'regisStat');
     Route::get('/regis-count', 'regisCount');
-    // Route::get('/part1-score', 'part1Score');
-    // Route::get('/part2-score', 'part2Score');
 });
-
-// Route::prefix('home')->controller(HomePageController::class)->group(function () {
-//     Route::get('/', 'index');
-//     // Route::get('/gen-uuid', 'generateUUIDV4');
-// });
 
 Route::prefix('template')->controller(JsonTemplateController::class)->group(function () {
     Route::get('/upload-documents', 'uploadDocumentsTemplate');
@@ -131,19 +124,3 @@ Route::prefix('template')->controller(JsonTemplateController::class)->group(func
     Route::get('/country-codes', 'countryCodes');
     Route::get('/all-company', 'allCompany');
 });
-
-// Route::prefix('admin')->controller(AdminController::class)->group(function () {
-//     Route::post('/company-detail', 'companyDetail');
-// });
-
-// Route::prefix('company')->controller(CompanyController::class)->group(function () {
-//     //     //! app/Http/Middleware/VerifyCsrfToken.php -> add '/auth/login'
-//     Route::post('/create-regis-id', 'createRegisId');
-//     Route::post('/upload-document', 'uploadDocument');
-//     Route::post('/test-post', 'testPost');
-//     Route::post('/add-company', 'addCompany');
-//     Route::post('/update-company', 'updateCompany');
-//     Route::get('/company-list', 'companyList');
-//     Route::get('/business-type', 'businessType');
-//     Route::get('/country-amount', 'countryAmount');
-// });
